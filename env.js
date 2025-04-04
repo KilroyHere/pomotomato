110b41a9378080b8fe635b7016f881eedabfa02d
KilroyHere <66572285+KilroyHere@users.noreply.github.com>
2025-04-04 22:52:36 +0000

    Deploying to gh-pages from @ KilroyHere/pomotomato@bd7c69db51f537fde7510d8e1271de7b81db55da 🚀

--- env.js
+++ env.js
@@ -1,14 +1,3 @@
-/**
- * IMPORTANT: This file contains a placeholder Spotify Client ID.
- * Before using this application, you must:
- * 
- * 1. Create your own Spotify Developer app at https://developer.spotify.com/dashboard/
- * 2. Replace "YOUR_SPOTIFY_CLIENT_ID_HERE" below with your actual client ID
- * 3. Make sure this file is in your .gitignore to avoid committing sensitive information
- */
-
-// This file is used for development
-// In production, it will be overwritten by GitHub Actions
 window._env_ = {
-  SPOTIFY_CLIENT_ID: "YOUR_SPOTIFY_CLIENT_ID_HERE"
-}; 
+  SPOTIFY_CLIENT_ID: ""
+};
