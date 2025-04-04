8bee91267e11eba76f9c32039a4be096909eb008
kilroyhere <aroo2001@gmail.com>
2025-04-04 16:59:26 -0700

    Update

--- public/env.template.js
+++ public/env.template.js
@@ -3,7 +3,7 @@
  * 
  * 1. Copy this file to env.js in the same directory
  * 2. Replace "YOUR_SPOTIFY_CLIENT_ID_HERE" with your actual Spotify Client ID
- * 3. Make sure env.js is in your .gitignore to avoid committing your client ID
+ * 3. Make sure env.js  is in your .gitignore to avoid committing your client ID
  */
 
 window._env_ = {
